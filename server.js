7e1d648f7810f4e52a547106b4b5296fbdef66ca
Tina Dessu <tinady2009@gmail.com>
2023-05-04 22:38:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ console.log('Hello world')
 // console.log(global)
 
 const os=require('os')
+const math=require('./math')
+
+console.log(math.add(67, 78))
 
 console.log(os.type())
 console.log(os.version())
