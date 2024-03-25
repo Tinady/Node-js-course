bacb25c73caa8803e2b01d64468be43aaf7fe245
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-03-25 21:09:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 const http= require('http')
 const fs= require('fs')
+const _ = require('lodash')
+
 
 const server= http.createServer((req,res)=>{
-    console.log(req.url, req.method)
+      const num= _.random( 0,20)
+      console.log(num)
+
+
     res.setHeader('contennt-Type', 'text/html')
+
+    let path="./views/"
+
+switch(req.url){
+    case '/':
+        path += 'index.html'
+        res.statusCode=200
+        break
+    
+    case '/about':
+        path += 'about.html'
+        res.statusCode=200
+        break
+    case '/about-me':
+            
+            res.statusCode=301
+            res.setHeader('location', '/about')
+            res.end()
+            break
+
+    default:
+         path += '404.html'
+         res.statusCode=404
+    
+
+}
 
     // send an html file
 
-    fs.readFile('./views/index.html', (err, data)=>{
+    fs.readFile( path , (err, data)=>{
         if(err)
         {console.log(err)
             res.end()}
@@ -21,6 +52,8 @@ const server= http.createServer((req,res)=>{
     
   
 })
+
+
 
 server.listen(3000, 'localhost', ()=>{
     console.log('listenining')
