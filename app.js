e33f9fa94d8dac95305ed6236684ea50e5b4ba08
Tina Dessu <88823243+Tinady@users.noreply.github.com>
2024-03-26 17:06:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,23 +2,33 @@ const express= require('express')
 
 const app=express();
 
+app.set('view engine', 'ejs')
+
 app.listen(3000);
 
 
 app.get('/', (req,res)=>{
-   res.sendFile('./views/index.html', {root:__dirname})
+   const blogs=[
+      {title: "Yoshi finds eggs", snippet:"Lorem ipsum dolor sit amet consectetur"},
+      {title: "Tina finds stars", snippet:"Lorem ipsum dolor sit amet consectetur"},
+      {title: "How to defeat bowser", snippet:"Lorem ipsum dolor sit amet consectetur"}
+      ]
+
+  res.render('index', {title:'Home', blogs})
 })
+
 
 
 app.get('/about', (req,res)=>{
-   res.sendFile('./views/about.html', {root:__dirname})
+   res.render('about')
+})
+
+app.get('/blogs/create', (req,res)=>{
+   res.render('create')
 })
 
 
-app.get('/about-us', (req, res)=>{
-res.redirect('/about')
-})
 
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html', {root:__dirname})
+   res.render('404')
 })
